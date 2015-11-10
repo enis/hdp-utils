77039f104a12206509f97ccd591643293cc54a14
Enis Soztutar <enis@apache.org>
2015-11-10 13:06:39 -0800

    Also grep for HDP branches in find_hdp_commit.sh

--- find_hdp_commit.sh
+++ find_hdp_commit.sh
@@ -15,13 +15,21 @@ fi
 
 COMMIT_TEXT=$1
 
-echo "****************************************************"
-echo "Greping commit ($COMMIT_TEXT) for HDP versions 2.1+";
-echo "****************************************************"
 
 HDP_VERSIONS="2.1 2.2 2.3 2.4"
+HDP_BRANCHES="2.1-maint 2.2-maint 2.3-maint"
+
+echo
+echo "****************************************************"
+echo "Git fetch -t";
+echo "****************************************************"
 
 git fetch -t 
+
+echo
+echo "****************************************************"
+echo "Greping commit ($COMMIT_TEXT) for HDP releases 2.1+";
+echo "****************************************************"
 
 for HDP_VERSION in $HDP_VERSIONS; do
   TAGS=`git tag | grep "HDP-$HDP_VERSION" | grep "tag"`
@@ -29,5 +37,14 @@ for HDP_VERSION in $HDP_VERSIONS; do
   for TAG in $TAGS; do
     echo "$TAG:"
     git log $TAG | grep $COMMIT_TEXT
-  done 
+  done
+done
+
+echo
+echo "****************************************************"
+echo "Greping commit ($COMMIT_TEXT) for HDP branches 2.1+";
+echo "****************************************************"
+for BRANCH in $HDP_BRANCHES; do
+  echo "$BRANCH:"
+  git log origin/$BRANCH | grep $COMMIT_TEXT
 done
